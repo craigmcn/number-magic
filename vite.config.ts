c8e46310ad4b524e2e42fa9af8ceb9536c6653a8
Craig McNaughton <therealcraigmcn@gmail.com>
2022-12-26 09:19:49 -0800

    Setup Netlify folders on build

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,15 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
+  build: {
+    rollupOptions: {
+      output: [
+        { dir: 'dist' },
+        { dir: 'dist/number-magic' }, // For Netlify subdirectory
+      ],
+    },
+  },
   server: {
     port: 3100,
   },
