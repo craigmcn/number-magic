20f27d964e659426d908974954fbe64ef8fd8bfb
Craig McNaughton <therealcraigmcn@gmail.com>
2022-12-27 11:58:18 -0800

    Add ESLint
    - run and fix

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,8 +1,8 @@
 export const sliceRandomElement = <T>(array: T[]) => {
-    const randomIndex = ~~(Math.random() * array.length)
-    const randomElement = array[randomIndex]
-    return {
-      element: randomElement,
-      array: array.filter((_, i) => i !== randomIndex)
-    }
-  }
+  const randomIndex = ~~(Math.random() * array.length);
+  const randomElement = array[randomIndex];
+  return {
+    element: randomElement,
+    array: array.filter((_, i) => i !== randomIndex),
+  };
+};
